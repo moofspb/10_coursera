fe7f8be8cc280ff892ac1b5869f06196c58d6570
moofspb <moofspb@mail.ru>
2017-02-24 23:21:49 +0000

    Add function get_courses_list and update requirements.txt

--- coursera.py
+++ coursera.py
@@ -1,7 +1,29 @@
+import random
+import requests
+import openpyxl
+from lxml import etree
+from bs4 import BeautifulSoup
+
+
+URL = 'https://www.coursera.org/sitemap~www~courses.xml'
+
+
+def get_page_content(url):
+    page = requests.get(url)
+    return page.content
+
+
+def get_random_list(original_list, new_list_length):
+    return random.sample(original_list, new_list_length)
 
 
 def get_courses_list():
-    pass
+    xml = get_page_content(URL)
+    parsed_xml = etree.XML(xml)
+    text_content = parsed_xml.xpath('//text()')
+    courses_urls = text_content[2::4]
+    random_courses = get_random_list(courses_urls, 20)
+    return random_courses
 
 
 def get_course_info(course_slug):
@@ -13,4 +35,4 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    pass
+    get_courses_list()
