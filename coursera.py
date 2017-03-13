777b8e00a94cf0e42d2b9671c2c2c63f9323d37d
Mikhail Kuntsevich <moofspb@gmail.com>
2017-03-13 17:56:56 +0300

    Remove function get_page_content, rewrite function collect_courses_data using list comprehention, change type of courses_data from list to namedtuple

--- coursera.py
+++ coursera.py
@@ -1,6 +1,7 @@
 import random
 import requests
 import argparse
+from collections import namedtuple
 from openpyxl import Workbook
 from lxml import etree
 from bs4 import BeautifulSoup
@@ -10,17 +11,13 @@ URL = 'https://www.coursera.org/sitemap~www~courses.xml'
 RANDOM_COURSES = 20
 
 
-def get_page_content(url):
-    page = requests.get(url)
-    return page.content
-
-
 def get_random_list(original_list, new_list_length):
     return random.sample(original_list, new_list_length)
 
 
-def get_courses_list():
-    xml = get_page_content(URL)
+def get_courses_list(url):
+    page = requests.get(url)
+    xml = page.content
     parsed_xml = etree.XML(xml)
     text_content = parsed_xml.xpath('//text()')
     courses_urls = text_content[2::4]
@@ -29,9 +26,10 @@ def get_courses_list():
 
 
 def get_course_info(course_slug):
-    course_page = get_page_content(course_slug)
+    page = requests.get(course_slug)
+    course_page = page.content
     page_soup = BeautifulSoup(course_page, 'lxml')
-    course_name = page_soup.find('h1', class_='title display-3-text').text
+    course_title = page_soup.find('h1', class_='title display-3-text').text
     course_lang = page_soup.find('div', class_='rc-Language').contents[1]
     course_start_date = page_soup.find('div',
                                        class_='startdate rc-StartDateString caption-text').text[7:]
@@ -40,21 +38,19 @@ def get_course_info(course_slug):
         course_rating = page_soup.find('div',
                                        class_='ratings-text bt3-hidden-xs').contents[1][20:]
     else:
-        course_rating = 'No data'
-    course_data = [course_name, course_lang, course_start_date,
-                   course_duration, course_rating]
-    return course_data
+        course_rating = None
+    course_data = namedtuple('course_data', ['course_title', 'course_lang', 'course_start_date',
+                                             'course_duration', 'course_rating'])
+    return course_data(course_title, course_lang, course_start_date,
+                       course_duration, course_rating)
 
 
 def collect_courses_data(course_urls):
-    courses_data = []
-    for url in course_urls:
-        courses_data.append(get_course_info(url))
-    return courses_data
-    
+    return [get_course_info(url) for url in course_urls]
+
 
 def output_courses_info_to_xlsx(filename, courses_data):
-    headers = ['Course Name', 'Language', 'Start Date',
+    headers = ['Title', 'Language', 'Start Date',
                'Duration(weeks)', 'User Rating']
     workbook = Workbook()
     worksheet = workbook.active
@@ -72,7 +68,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print('The data will be saved in {}.xlsx'.format(args.filename))
     print('Getting courses list...')
-    courses_urls = get_courses_list()
+    courses_urls = get_courses_list(URL)
     print('Collecting courses data...')
     courses_data = collect_courses_data(courses_urls)
     output_courses_info_to_xlsx(args.filename, courses_data)
